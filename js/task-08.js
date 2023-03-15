8aae699b2a4464f1ccc3b6f379527533b7204251
SkliarovaNastya <116487853+8Anastasiia8@users.noreply.github.com>
2023-03-15 11:48:22 +0200

    Changes 1

--- js/task-08.js
+++ js/task-08.js
@@ -5,19 +5,15 @@ const object = {
 };
 
 form.addEventListener("submit", handleSubmit);
-
 function handleSubmit(event) {
   event.preventDefault();
   const {
     elements: { email, password },
   } = event.currentTarget;
-
-  if (email.value === "" || password.value === "") {
-    alert("Please fill in all the fields!");
-  }
   object.login = email.value;
   object.password = password.value;
-
-  console.log(object);
+  if (email.value === "" || password.value === "") {
+    alert("Please fill in all the fields!");
+  } else console.log(object);
   event.currentTarget.reset();
 }
